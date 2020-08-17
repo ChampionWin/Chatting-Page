ff9713bd1c94f1aa5f1f8cc7cd2ff96b0b0fabb8
ChampionWin <championwillwin6@outlook.com>
2020-08-17 01:38:44 -0400

    remove a space

--- server/app.js
+++ server/app.js
@@ -1,9 +1,6 @@
 const WebSocket = require("ws");
-
 const wss = new WebSocket.Server({ port: 8989 });
-
 const users = [];
-
 const broadcast = (data, ws) => {
   wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN && client !== ws) {
